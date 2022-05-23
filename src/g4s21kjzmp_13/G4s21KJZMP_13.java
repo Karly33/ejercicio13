f8f932fe77ca57b879d9124d3fce8d38b23ae852
JM <JM@KARLY->
2022-05-22 23:36:58 -0700

    ejercicio13

---
+++ src/g4s21kjzmp_13/G4s21KJZMP_13.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package g4s21kjzmp_13;
+
+/**
+ *
+ * @author JM
+ */
+public class G4s21KJZMP_13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    new JFPrincipal().setVisible(true);
+    
+    
+}
+}
